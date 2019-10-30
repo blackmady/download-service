8a213d1e6ece57a2b58ab6ca2223c402943e8b8b
liukun <liukun@swipal.com>
2019-10-30 18:37:16 +0800

    数据库操作

--- website/server/db.js
+++ website/server/db.js
@@ -11,40 +11,28 @@ fs.exists(file,exists=>{
   );`;
   sqliteDB.createTable(createFilesTableSql);
 })
-// /// insert data.
-// const tileData = [[1, 10, 10], [1, 11, 11], [1, 10, 9], [1, 11, 9]];
-// const insertTileSql = "insert into tiles(level, column, row) values(?, ?, ?)";
-// sqliteDB.insertData(insertTileSql, tileData);
-// /// query data.
-// const querySql = 'select * from tiles where level = 1 and column >= 10 and column <= 11 and row >= 10 and row <=11';
-// sqliteDB.queryData(querySql, dataDeal);
-// /// update data.
-// const updateSql = 'update tiles set level = 2 where level = 1 and column = 10 and row = 10';
-// sqliteDB.executeSql(updateSql);
-// /// query data after update.
-// querySql = "select * from tiles where level = 2";
-// sqliteDB.queryData(querySql, dataDeal);
-// sqliteDB.close();
-// function dataDeal(objects) {
-//   for (let i = 0; i < objects.length; ++i) {
-//     console.log(objects[i]);
-//   }
-// }
 
 class DB{
   constructor(table){
     this.table=table
   }
   add(data){
-    let keys=Object.keys(data)
-    let values=Object.values(data)
-    let nv=values.map(item=>typeof item==='string'?`"${item}"`:item)
-    let sql=`insert into ${this.table} (${keys.join()}) values(${nv})`
+    const keys=Object.keys(data)
+    const values=Object.values(data)
+    const nv=values.map(item=>typeof item==='string'?`"${item}"`:item)
+    const sql=`insert into ${this.table} (${keys.join()}) values(${nv})`
     return this.exec(sql)
   }
   list(){
-    let sql=`select * from ${this.table}`
+    const sql=`select * from ${this.table}`
     return this.exec(sql)
+  }
+  update(){
+    const sql = `update ${this.table} set level = 2 where level = 1 and column = 10 and row = 10`
+    return this.exec(sql)
+  }
+  delete(){
+    
   }
   exec(sql){
     return new Promise((resolve,reject)=>{
